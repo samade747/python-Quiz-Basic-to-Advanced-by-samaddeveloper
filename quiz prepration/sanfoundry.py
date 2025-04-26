a821fc06bad8bfb02e94262ae6c11bf307e5da64
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-26 18:54:16 +0500

    Create sanfoundry.py

---
+++ quiz prepration/sanfoundry.py
@@ -0,0 +1,21 @@
+1. Is Python case sensitive when dealing with identifiers?
+a) yes
+b) no
+c) machine dependent
+d) none of the mentioned
+View Answer
+
+Answer: a
+Explanation: Case is always significant while dealing with identifiers in python.
+
+
+
+2. What is the maximum possible length of an identifier?
+a) 31 characters
+b) 63 characters
+c) 79 characters
+d) none of the mentioned
+View Answer
+
+Answer: d
+Explanation: Identifiers can be of any length.
