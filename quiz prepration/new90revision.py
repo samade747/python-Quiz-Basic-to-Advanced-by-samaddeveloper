ddd81c4680d469ee1810d13e983a853ee763dd41
Abdul Samad <132301326+samade747@users.noreply.github.com>
2025-04-28 11:54:46 +0500

    Update new90revision.py

---
+++ quiz prepration/new90revision.py
@@ -0,0 +1,1060 @@
+https://github.com/samade747
+
+Added most complex & latest quiz kindly read it once 90 questions 
+
+Complex Quiz for Panaverse Python Sections 01 to 09
+What is the output of the following Python code?
+
+
+
+print(-3**2)
+
+A) 9
+ B) -9
+ C) -6
+ D) 6
+ Correct Answer: B
+What will be printed by this code snippet?
+
+
+a = b = 5
+b = 3
+print(a)
+
+A) 5
+ B) 3
+ C) 0
+ D) Error
+ Correct Answer: A
+Determine the output of the code below:
+
+
+x = 5
+y = 2 * x
+x = 3
+print(y)
+
+A) 6
+ B) 9
+ C) 10
+ D) 15
+ Correct Answer: C
+What does this code output?
+
+
+x = 1000
+y = 1000
+print(x is y)
+
+A) True
+ B) False
+ C) Error
+ D) Maybe True or False (implementation detail)
+ Correct Answer: B
+What will the following code print?
+
+
+
+print("Hello", "World", sep="-", end="!")
+
+A) Hello World!
+ B) Hello-World!
+ C) Hello-World
+ D) HelloWorld!
+ Correct Answer: B
+What happens when this code is executed?
+
+
+if True:
+print("Hello")
+
+A) Hello
+ B) IndentationError
+ C) "Hello" printed twice
+ D) SyntaxError
+ Correct Answer: B
+What is the output of this code?
+
+
+print(type(True))
+
+A) <class 'bool'>
+ B) <class 'int'>
+ C) <class 'str'>
+ D) <class 'boolean'>
+ Correct Answer: A
+What is the result of evaluating type(5/2)?
+
+
+
+print(type(5/2))
+
+A) int
+ B) float
+ C) complex
+ D) TypeError
+ Correct Answer: B
+What does type(None) evaluate to?
+
+
+
+print(type(None))
+
+A) NoneType
+ B) <class 'NoneType'>
+ C) <class 'None'>
+ D) None
+ Correct Answer: B
+What will be printed by the following code?
+
+
+print(True + False)
+
+A) 1
+ B) 0
+ C) True
+ D) False
+ Correct Answer: A
+What is the output of this code?
+
+
+print(type(3+4j))
+
+A) <class 'complex'>
+ B) <class 'float'>
+ C) <class 'int'>
+ D) numbers.Number
+ Correct Answer: A
+What does the following print?
+
+
+
+print(int("101", 2))
+
+A) 5
+ B) 101
+ C) 2
+ D) Error
+ Correct Answer: A
+What is the output of this rounding operation?
+
+
+print(round(2.675, 2))
+
+A) 2.67
+ B) 2.68
+ C) 2.7
+ D) 2.675
+ Correct Answer: A
+What does this cast output?
+
+
+print(int(2.9))
+
+A) 2
+ B) 3
+ C) 2.9
+ D) Error
+ Correct Answer: A
+What is printed by this code?
+
+
+print(ord('A'))
+
+A) 65
+ B) 'A'
+ C) 64
+ D) Error
+ Correct Answer: A
+What is the result of this comparison?
+
+
+print(0.1 + 0.2 == 0.3)
+
+A) True
+ B) False
+ C) 0.30000000000000004
+ D) Error
+ Correct Answer: B
+What will be printed by this arithmetic expression?
+
+
+
+print(2 + 3 * 4)
+
+A) 20
+ B) 14
+ C) 10
+ D) 24
+ Correct Answer: B
+Determine the output:
+
+
+print(not True or False)
+
+A) True
+ B) False
+ C) None
+ D) SyntaxError
+ Correct Answer: B
+What is the output of this code involving bitwise operators?
+
+
+print((5 & 3) | 2)
+
+A) 1
+ B) 7
+ C) 3
+ D) 6
+ Correct Answer: C
+What does this left-shift expression print?
+
+
+print(1 << 3)
+
+A) 3
+ B) 4
+ C) 8
+ D) 16
+ Correct Answer: C
+Which of the following is not a Python keyword? A) class
+ B) try
+ C) int
+ D) finally
+ Correct Answer: C
+
+
+What is the result of the chained comparison?
+
+
+print(1 < 2 < 3)
+
+A) True
+ B) False
+ C) 1
+ D) 0
+ Correct Answer: A
+What will be printed by this logical expression?
+
+
+print([] and 3 or 4)
+
+A) []
+ B) 3
+ C) 4
+ D) None
+ Correct Answer: C
+What is the output of this membership test?
+print('b' in 'alphabet')
+
+A) True
+ B) False
+ C) Error
+ D) None
+ Correct Answer: A
+Consider the following code. What is printed?
+
+
+a = [1,2]
+b = [1,2]
+print(a == b, a is b)
+
+A) True False
+ B) False True
+ C) True True
+ D) False False
+ Correct Answer: A
+What is the result of this augmented assignment?
+
+
+a = 5
+a += 3 * 2
+print(a)
+
+A) 11
+ B) 16
+ C) 10
+ D) 9
+ Correct Answer: A
+What does this slicing operation output?
+
+
+s = "abcdef"
+print(s[2:5])
+
+A) "cde"
+ B) "bcd"
+ C) "cdef"
+ D) "de"
+ Correct Answer: A
+What is printed by this code?
+s = "abcdef"
+print(s[-2])
+
+A) 'e'
+ B) 'f'
+ C) 'd'
+ D) 'b'
+ Correct Answer: A
+Determine the output of this slicing:
+
+
+s = "abcdef"
+print(s[1:5:2])
+
+A) "bd"
+ B) "ace"
+ C) "bdf"
+ D) "bcde"
+ Correct Answer: A
+What will this join operation output?
+
+
+print("-".join(["a","b","c"]))
+
+A) "a-b-c"
+ B) "abc"
+ C) "a b c"
+ D) Error
+ Correct Answer: A
+What happens when this code runs?
+
+
+
+s = "hello"
+s[0] = 'H'
+
+A) s becomes "Hello"
+ B) TypeError
+ C) Nothing happens
+ D) SyntaxError
+ Correct Answer: B
+What is the output of the following f-string code?
+
+
+
+name = "Alice"
+print(f"Hello, {name}!")
+
+A) Hello, Alice!
+ B) Hello, {name}!
+ C) Hello, name!
+ D) SyntaxError
+ Correct Answer: A
+What does this raw string print?
+
+
+
+print(r"\n")
+
+A) \n
+ B) (newline)
+ C) "\n"
+ D) Error
+ Correct Answer: A
+What is printed by this formatting example?
+
+
+val = 123.456
+print(f"{val:.2f}")
+
+A) 123.46
+ B) 123.45
+ C) 123.00
+ D) 12.35
+ Correct Answer: A
+What is the result of this string operation?
+
+
+
+print("4" * 3)
+
+A) 444
+ B) 12
+ C) 334
+ D) 4
+ Correct Answer: A
+What happens when this code is executed?
+
+
+
+print(int("3.14"))
+
+A) 3
+ B) 3.14
+ C) ValueError
+ D) 314
+ Correct Answer: C
+What will the following code output?
+
+
+if []:
+    print("Yes")
+else:
+    print("No")
+
+A) Yes
+ B) No
+ C) Error
+ D) None
+ Correct Answer: B
+What is the output of this code involving range?
+
+
+print(list(range(1, 5, 2)))
+
+A) [1, 3]
+ B) [1, 2, 3, 4]
+ C) [1, 2, 3]
+ D) [2, 4]
+ Correct Answer: A
+Determine the output of the following while loop:
+i = 0
+while i < 2:
+    print(i, end=" ")
+    i += 1
+else:
+    print("done")
+
+A) 0 1 done
+ B) 0 1
+ C) done
+ D) 0 done
+ Correct Answer: A
+What does this for-else loop print?
+
+
+for i in [1, 2]:
+    print(i, end=' ')
+else:
+    print("done")
+
+A) 1 2 done
+ B) 1 2
+ C) done
+ D) Error
+ Correct Answer: A
+What is printed when this loop runs?
+for i in [1, 2, 3]:
+    if i == 2:
+        break
+    print(i, end=' ')
+
+A) 1 2 3
+ B) 1 2
+ C) 1 3
+ D) 1
+ Correct Answer: D
+What will this code output?
+
+
+
+for i in [1, 2, 3]:
+    if i == 2:
+        continue
+    print(i, end=' ')
+
+A) 1 2 3
+ B) 1 2
+ C) 1 3
+ D) 2 3
+ Correct Answer: C
+What does this nested if print?
+
+
+x = 5
+if x > 2:
+    if x < 10:
+        print("B")
+    else:
+        print("C")
+else:
+    print("A")
+
+A) A
+ B) B
+ C) C
+ D) Error
+ Correct Answer: B
+Determine the output of this logical expression:
+
+
+print(True and False or True)
+
+A) True
+ B) False
+ C) None
+ D) Error
+ Correct Answer: A
+What is printed by this truthiness test?
+
+
+
+a = ""
+if a:
+    print("A")
+elif a is None:
+    print("None")
+else:
+    print("Empty")
+
+A) A
+ B) None
+ C) Empty
+ D) Error
+ Correct Answer: C
+What does this ternary expression output?
+
+
+print("Even" if 4 % 2 == 0 else "Odd")
+
+A) Even
+ B) Odd
+ C) 0
+ D) 4
+ Correct Answer: A
+What is the output of this list indexing?
+
+
+lst = [10, 20, 30]
+print(lst[1])
+
+A) 10
+ B) 20
+ C) 30
+ D) Error
+ Correct Answer: B
+What does this print?
+
+
+lst = [10, 20, 30]
+print(lst[-1])
+
+A) 10
+ B) 20
+ C) 30
+ D) Error
+ Correct Answer: C
+What is the output of this list slicing?
+
+
+lst = [1, 2, 3, 4, 5]
+print(lst[1:4])
+
+A) [1, 2, 3]
+ B) [2, 3, 4]
+ C) [2, 3, 4, 5]
+ D) [1, 2]
+ Correct Answer: B
+Determine the output of this list multiplication:
+
+
+lst = [1, 2] * 3
+print(lst)
+
+A) [1, 2, 1, 2, 1, 2]
+ B) [3, 6]
+ C) [1, 2, 3]
+ D) [1, 2]
+ Correct Answer: A
+What does this code output?
+
+
+lst = [1, 2]
+lst.append([3, 4])
+print(lst)
+
+A) [1, 2, 3, 4]
+ B) [1, 2, [3, 4]]
+ C) [1, 2, 3]
+ D) Error
+ Correct Answer: B
+What is printed after this code?
+
+
+lst = [1, 2]
+lst.extend([3, 4])
+print(lst)
+
+A) [1, 2, 3, 4]
+ B) [1, 2, [3, 4]]
+ C) [1, 2, 3]
+ D) [1, 2]
+ Correct Answer: A
+What happens when running this code?
+
+
+
+tpl = (1, 2, 3)
+tpl[0] = 5
+
+A) (5, 2, 3)
+ B) [5, 2, 3]
+ C) TypeError
+ D) SyntaxError
+ Correct Answer: C
+What is the output of this dictionary access?
+
+
+d = {'a': 1, 'b': 2}
+print(d['b'])
+
+A) 1
+ B) 2
+ C) 'b'
+ D) Error
+ Correct Answer: B
+What does this print?
+
+
+d = {'a': 1, 'b': 2}
+print(list(d.keys()))
+
+A) ['a', 'b']
+ B) [1, 2]
+ C) Error
+ D) 'a b'
+ Correct Answer: A
+What is printed by this dictionary example?
+
+
+d = {'a':1, 'a':2}
+print(d['a'])
+
+A) 1
+ B) 2
+ C) Error
+ D) 'a'
+ Correct Answer: B
+Determine the output of this function call:
+def f(item, lst=[]):
+    lst.append(item)
+    return lst
+
+print(f(1))
+print(f(2))
+
+A) [1] [2]
+ B) [1] [1, 2]
+ C) [1, 2] [1, 2]
+ D) [1, 1, 2] [1, 2]
+ Correct Answer: B
+What is the result of unpacking this list?
+
+
+a, b = [1, 2]
+print(a, b)
+
+A) 1 2
+ B) [1, 2]
+ C) (1, 2)
+ D) Error
+ Correct Answer: A
+What does this print?
+
+
+print(len({1, 2, 2, 3}))
+
+A) 3
+ B) 4
+ C) Error
+ D) 2
+ Correct Answer: A
+What is the output of this set union?
+
+
+print({1, 2} | {2, 3})
+
+A) {1, 2, 3}
+ B) {1, 3}
+ C) {2}
+ D) Error
+ Correct Answer: A
+What is the output of this set intersection?
+
+
+
+print({1, 2, 3} & {2, 4})
+
+A) {2}
+ B) {1, 4}
+ C) {2, 4}
+ D) {}
+ Correct Answer: A
+What does this print?
+
+
+print({1, 2, 3} - {2})
+
+A) {1, 3}
+ B) {2}
+ C) {1, 2, 3}
+ D) {}
+ Correct Answer: A
+Determine the output of this symmetric difference:
+
+
+print({1, 2} ^ {2, 3})
+
+A) {1, 3}
+ B) {2}
+ C) {1, 2, 3}
+ D) {}
+ Correct Answer: A
+What happens when this code runs?
+s = {1, 2}
+s.remove(3)
+
+A) None
+ B) KeyError
+ C) {1, 2}
+ D) {1, 2, 3}
+ Correct Answer: B
+What is the result of this operation?
+
+
+s = set()
+s.add([1, 2])
+
+A) None
+ B) TypeError
+ C) {1, 2}
+ D) {[1, 2]}
+ Correct Answer: B
+What does this print?
+
+
+
+print({1,2}.issubset({1,2,3}))
+
+A) True
+ B) False
+ C) Error
+ D) None
+ Correct Answer: A
+What is the output of this frozenset example?
+
+
+
+f = frozenset([1, 2, 2, 3])
+print(len(f))
+
+A) 3
+ B) 4
+ C) 2
+ D) Error
+ Correct Answer: A
+What is printed by this code?
+
+
+import math
+print(math.sqrt(16))
+
+A) 4.0
+ B) 4
+ C) Error
+ D) 16
+ Correct Answer: A
+What is the output of this import example?
+
+
+from math import pi
+print(pi)
+
+A) 3.14159...
+ B) pi
+ C) Error
+ D) 3.14
+ Correct Answer: A
+What happens when running this line?
+
+
+
+print(sin(0))
+
+A) 0
+ B) 1
+ C) NameError
+ D) SyntaxError
+ Correct Answer: C
+What will be printed?
+
+
+def f():
+    a = 5
+
+x = f()
+print(x)
+
+A) 5
+ B) None
+ C) Error
+ D) 0
+ Correct Answer: B
+What is the output of this function call?
+
+
+
+def f(x, y=2):
+    return x + y
+print(f(3))
+
+A) 5
+ B) 32
+ C) Error
+ D) 3
+ Correct Answer: A
+Determine the output of this code:
+
+
+
+def f(x, lst=[]):
+    lst.append(x)
+    return lst
+
+print(f(1))
+print(f(2, []))
+
+A) [1] [1, 2]
+ B) [1] [2]
+ C) [1, 2] [2]
+ D) [1] [1]
+ Correct Answer: B
+What does this code print?
+
+
+
+def add(*nums):
+    return sum(nums)
+print(add(1, 2, 3))
+
+A) 6
+ B) 123
+ C) Error
+ D) (1,2,3)
+ Correct Answer: A
+What is the output of this function?
+
+
+
+def f(**kwargs):
+    return kwargs
+print(f(a=1, b=2))
+
+A) {'a': 1, 'b': 2}
+ B) [1, 2]
+ C) Error
+ D) None
+ Correct Answer: A
+What does this lambda expression output?
+
+
+f = lambda x: x * 2
+print(f(5))
+
+A) 10
+ B) 55
+ C) x2
+ D) Error
+ Correct Answer: A
+What is printed by this map example?
+
+
+
+print(list(map(lambda x: x*2, [1, 2, 3])))
+
+A) [2, 4, 6]
+ B) [1, 2, 3]
+ C) [1, 4, 9]
+ D) Error
+ Correct Answer: A
+What is the output of this recursive function?
+
+
+
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fact(n-1)
+print(fact(3))
+
+A) 6
+ B) 9
+ C) 1
+ D) 120
+ Correct Answer: A
+What does the following code print?
+
+
+x = 5
+def f():
+    x = 3
+f()
+print(x)
+
+A) 5
+ B) 3
+ C) Error
+ D) None
+ Correct Answer: A
+What is the output of this code?
+
+
+
+x = 5
+def f():
+    global x
+    x = 3
+f()
+print(x)
+
+A) 5
+ B) 3
+ C) Error
+ D) None
+ Correct Answer: B
+Determine the output:
+
+
+def greet():
+    print("Hello")
+g = greet
+g()
+
+A) Hello
+ B) greet
+ C) Error
+ D) None
+ Correct Answer: A
+What is the result of this try/except block?
+
+
+try:
+    x = 1/0
+except ZeroDivisionError:
+    print("Error")
+
+A) Error
+ B) 0
+ C) 1
+ D) 0.5
+ Correct Answer: A
+What is printed by this exception handling?
+
+
+try:
+    x = int("a")
+except:
+    print("Oops")
+
+A) Oops
+ B) 0
+ C) 1
+ D) None
+ Correct Answer: A
+What will the following code print?
+
+
+try:
+    x = int("1")
+except:
+    print("fail")
+else:
+    print("success")
+
+A) success
+ B) fail
+ C) Error
+ D) nothing
+ Correct Answer: A
+What is the output of this try/finally?
+
+
+
+try:
+    print(1)
+finally:
+    print(2)
+
+A) 1 2
+ B) 1
+ C) 2
+ D) None
+ Correct Answer: A
+What happens here?
+
+
+
+try:
+    print(1/0)
+finally:
+    print(2)
+
+A) 2 and ZeroDivisionError
+ B) 1
+ C) 2
+ D) ZeroDivisionError
+ Correct Answer: A
+What does this function return?
+
+
+
+def f():
+    try:
+        return 1
+    finally:
+        return 2
+print(f())
+
+A) 1
+ B) 2
+ C) None
+ D) Error
+ Correct Answer: B
+What will be printed?
+
+
+
+try:
+    pass
+except:
+    print("No")
+else:
+    print("Yes")
+
+A) Yes
+ B) No
+ C) YesNo
+ D) Error
+ Correct Answer: A
+Determine the output:
+
+
+try:
+    x = 10
+except:
+    x = 5
+finally:
+    print(x)
+
+A) 10
+ B) 5
+ C) Error
+ D) None
+ Correct Answer: A
+What is printed by this assertion example?
+
+
+python
+CopyEdit
+try:
+    assert False, "oops"
+except AssertionError as e:
+    print(e)
+
+A) oops
+ B) AssertionError
+ C) nothing
+ D) Error
+ Correct Answer: A
